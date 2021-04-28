421f8e6efacd30000aa9218dbfedc0528451dec2
deliaqi <delia.qi@live.cn>
2021-04-27 21:04:37 -0400

    add reminder UI and functionality for adding new record

--- lib/mypet.dart
+++ lib/mypet.dart
@@ -26,6 +26,9 @@ class MyPetPage extends StatefulWidget {
 
 class _MyPetPageState extends State<MyPetPage> {
   final DataRepository repository = DataRepository();
+  List<bool> reminderVal = [true, false, true];
+  List<String> reminderText = ['Walking', 'Prepare meal', 'Playing'];
+  List<String> reminderTime = ['8:00am', '11:00am', '5:00pm'];
 
   int _counter = 0;
 
@@ -43,19 +46,17 @@ class _MyPetPageState extends State<MyPetPage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final textTheme = Theme.of(context).textTheme;
+    final colorScheme = Theme.of(context).colorScheme;
     return DefaultTabController(
       length: 2,
       child: Scaffold(
+        // backgroundColor: colorScheme.secondary,
         appBar: AppBar(
-          title: Text('Pet Records      '),
-          automaticallyImplyLeading: false,
-          bottom: TabBar(
+          backgroundColor: colorScheme.primary,
+          // title: Text('Pet Records      '),
+          // automaticallyImplyLeading: true,
+          title: TabBar(
             indicatorColor: Colors.white,
             tabs: [
               Tab(text: 'Lucas'),
@@ -65,45 +66,119 @@ class _MyPetPageState extends State<MyPetPage> {
         ),
         body: TabBarView(
           children: [
-            Center(child: buildTabBarView('Lucas')),
-            Center(child: buildTabBarView('Kitty')),
+            Center(child: buildPetPage('Lucas', context)),
+            Center(child: buildPetPage('Kitty', context)),
           ],
         ),
       ),
     );
   }
 
-  Widget buildTabBarView(String tab) {
+  Widget buildPetPage(String petName, BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        // backgroundColor: colorScheme.secondary,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          // shadowColor: Colors.grey,
+          // title: Text('Pet Records      '),
+          // automaticallyImplyLeading: true,
+          title: TabBar(
+            indicatorColor: Colors.grey,
+            tabs: [
+              Tab(text: 'Record'),
+              Tab(text: 'Reminder'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(child: buildRecordList(petName)),
+            Center(child: buildReminderList(petName)),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: colorScheme.primary,
+          foregroundColor: Colors.black,
+          onPressed: () {
+            setState(() {
+              _addRecord(context);
+            });
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+
+  _addRecord(BuildContext parentContext) async {
+    return showDialog<Null>(
+      context: parentContext,
+      barrierDismissible: false,
+
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Create a new record'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              TextFormField(
+                initialValue: '04/27/2021',
+                maxLength: 20,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.favorite),
+                  labelText: 'Date',
+                ),
+              ),
+              TextFormField(
+                initialValue: '10',
+                maxLength: 4,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.favorite),
+                  labelText: 'Weight',
+                ),
+              ),
+              TextFormField(
+                initialValue: '200',
+                maxLength: 4,
+                decoration: InputDecoration(
+                  icon: Icon(Icons.favorite),
+                  labelText: 'Calories',
+                ),
+              ),
+            ],
+          ),
+          actions: [
+            FlatButton(
+              textColor: shrinePink400,
+              onPressed: () => Navigator.pop(context),
+              child: Text('Confirm'),
+            ),
+            FlatButton(
+              textColor: shrinePink400,
+              onPressed: () => Navigator.pop(context),
+              child: Text('Cancel'),
+            ),
+          ],
+        );
+      }
+    );
+  }
+
+  Widget buildRecordList(String tab) {
     return ListView(
       children: [
-        buildCard('04/13/2021'),
-        ListTile(
-          title: Text('Weight'),
-          leading: Icon(Icons.tour),
-          trailing: Text('9.49lbs'),
-        ),
-        ListTile(
-          title: Text('Calories'),
-          leading: Icon(Icons.leaderboard),
-          trailing: Text('97 CAL'),
-        ),
-        ListTile(
-          title: Text('Next Deworming'),
-          leading: Icon(Icons.pan_tool),
-          trailing: Text('03/20/2021'),
-        ),
-        ListTile(
-          title: Text('Next Vaccination'),
-          leading: Icon(Icons.verified_user),
-          trailing: Text('11/24/2021'),
-        ),
+        buildRecord('04/13/2021'),
+        buildRecord('04/14/2021'),
       ],
     );
   }
 
 
-
-  Widget buildCard(String tab) {
+  Widget buildRecord(String tab) {
     return Card(
       clipBehavior: Clip.antiAlias,
       child: Column(
@@ -112,10 +187,54 @@ class _MyPetPageState extends State<MyPetPage> {
             // leading: Text("04/13/2021"),
             title: Center(child: Text(tab, style: TextStyle(fontSize: 25))),
           ),
+          Image.network('https://www.ctvnews.ca/polopoly_fs/1.5098407.1599687805!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg'),
+          ListTile(
+            title: Text('Weight'),
+            leading: Icon(Icons.tour),
+            trailing: Text('9.49lbs'),
+          ),
+          ListTile(
+            title: Text('Calories'),
+            leading: Icon(Icons.leaderboard),
+            trailing: Text('97 CAL'),
+          ),
+          ListTile(
+            title: Text('Next Deworming'),
+            leading: Icon(Icons.pan_tool),
+            trailing: Text('03/20/2021'),
+          ),
+          ListTile(
+            title: Text('Next Vaccination'),
+            leading: Icon(Icons.verified_user),
+            trailing: Text('11/24/2021'),
+          ),
           ButtonBar(
             alignment: MainAxisAlignment.start,
           ),
-          Image.network('https://www.ctvnews.ca/polopoly_fs/1.5098407.1599687805!/httpImage/image.jpg_gen/derivatives/landscape_1020/image.jpg'),
+        ],
+      ),
+    );
+  }
+
+  Widget buildReminderList(String tab) {
+    int count = 3;
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          for (int i = 0; i < count; i++)
+            ListTile(
+              title: Text('${reminderText[i]}'),
+              leading: Switch(
+                value: reminderVal[i],
+                onChanged: i == count ? null
+                      : (bool value) {
+                  setState(() {
+                    reminderVal[i] = value;
+                  });
+                },
+              ),
+              trailing: Text('${reminderTime[i]}'),
+            ),
         ],
       ),
     );
